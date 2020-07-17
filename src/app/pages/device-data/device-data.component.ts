c3b56a222ebfc1cc7947338fb337340f6e74afe8
alfaz-003 <alfazvahora009@gmail.com>
2020-07-17 12:47:07 +0530

    updated project

--- src/app/pages/device-data/device-data.component.ts
+++ src/app/pages/device-data/device-data.component.ts
@@ -1,32 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
-import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
-import {headerlocal, PROXYURL, API_URL} from 'src/app/app.constant'
+import { headerlocal, PROXYURL, API_URL } from 'src/app/app.constant'
 import { DevicedataService } from 'src/app/service/devicedata.service';
-import  { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
 
 export class tdata {
   constructor(
-  public telemetryid,
-  public deviceid,
-  public temperature,
-  public humidity,
-  public enqueuedtime)
-  {
+    public telemetryid,
+    public deviceid,
+    public temperature,
+    public humidity,
+    public enqueuedtime) {
 
   }
 }
 export class chrt {
   constructor(
-  public count,
-  public days,
-  public temp,
-  public hum
-  )
-  {
+    public count,
+    public days,
+    public temp,
+    public hum
+  ) {
 
   }
 }
@@ -39,36 +37,36 @@ export class chrt {
 
 
 
- 
+
 
 export class DeviceDataComponent {
-   
-  teledata:Array<any>;
-  totalRecords : Number
-  page : Number=1
+
+  teledata: Array<any>;
+  totalRecords: Number
+  page: Number = 1
 
   constructor(private httpClient: HttpClient,
-    private deviceDataService:DevicedataService) { 
-      this.teledata = new Array<any>()
-    
+    private deviceDataService: DevicedataService) {
+    this.teledata = new Array<any>()
+
     this.deviceDataService.getAllTelemetryDevice().subscribe(
       data => {
-         this.teledata = data
+        this.teledata = data
         //console.log(this.teledata)
       },
       error => {
         console.log(error)
       }
-      
+
     )
-    
+
 
 
 
   }
 
-  
 
- 
+
+
 
 }
